db9757142eb43326b1b4d8981e0b204039090e88
Gabriel Queiroz <gabriel-cso@hotmail.com>
2023-07-09 23:29:21 -0300

    Structuring Domain classes

---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Room
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Level { get; set; }
+    public bool InMaintenance { get; set; }
+    public bool IsAvailable
+    {
+        get
+        {
+            if (this.InMaintenance || this.HasGuest) return false;
+            return true;
+        }
+    }
+    public bool HasGuest
+    {
+        //TODO: Verificar se existem Bookins abertos para esta Room
+        get{ return true;}
+    }
+}
---
+++ BookingService/Core/Domain/Enums/EStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums;
+
+public enum EStatus
+{
+    Created = 0,
+    Paid = 1,
+    Finished = 2,
+    Canceled = 3,
+    Refounded = 4
+}
