8b92449390ee64e814ae25f6b3b97cd8c9f75e4d
Gabriel Queiroz <gabriel-cso@hotmail.com>
2023-07-10 22:31:42 -0300

    Adicionando pattern state machine

--- BookingService/Core/Domain/Entities/Booking.cs
+++ BookingService/Core/Domain/Entities/Booking.cs
@@ -15,4 +15,17 @@ public class Booking
     public DateTime End { get; set; }
     private EStatus Status { get; set; }
     public EStatus CurrentStatus { get { return this.Status; } }
+
+    public void ChangeState(EAction action)
+    {
+        this.Status = (this.Status, action) switch
+        {
+            (EStatus.Created,  EAction.Pay)      => EStatus.Paid,
+            (EStatus.Created,  EAction.Cancel)   => EStatus.Canceled,
+            (EStatus.Paid,     EAction.Finish)   => EStatus.Finished,
+            (EStatus.Paid,     EAction.Refound)  => EStatus.Refounded,
+            (EStatus.Canceled, EAction.Reopen)   => EStatus.Created,
+              _ => this.Status
+        };
+    }
 }
---
+++ BookingService/Core/Domain/Enums/EAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums;
+
+public enum EAction
+{
+    Pay = 0,
+    Finish = 1, // after paid and used
+    Cancel = 2, // can nerver be paid
+    Refound = 3, // Paid then refound
+    Reopen = 4 // canceled
+}
