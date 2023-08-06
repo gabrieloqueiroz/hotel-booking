38ab78307eb71c7ca7ae0f4955da0cdfd36a3615
Gabriel Queiroz <gabriel-cso@hotmail.com>
2023-08-06 19:59:30 -0300

    Adding room port to in Core

---
+++ BookingService/Core/Application/Room/Ports/In/IRoomManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Room.Ports.In;
+
+public interface IRoomManager
+{
+    Task<RoomResponse> Create(CreateRoomRequest request);
+    Task<RoomResponse> Get(int id);
+}
